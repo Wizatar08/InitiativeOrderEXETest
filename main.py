5060ee4e36242433f128ffc08a051fddc9fe85ce
_Wizatar08 <kiyoshijim88@gmail.com>
2023-11-24 12:01:50 -0700

    Added the new combat tab
    Also got rid of all the color stuff because it was starting to look messy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import PySimpleGUI as gui
 import random
 
-
+MENU_OPTIONS = ["Add Entity", "Run Combat"]
 
 class Entity:
 
@@ -47,14 +47,19 @@ def main():
     window = setUp()
     run(window)
 
-def collapsable(layout, k, size=(None, None), visible=True):
-    return gui.pin(gui.Column(layout, size=size, visible=visible, key=k))
+def collapsable(layout, key, size=(None, None), visible=True):
+    return gui.pin(gui.Column(layout, size=size, visible=visible, key=key))
 
 def setUp():
+    topBar = [
+        [gui.Text("Current Mode:"), gui.Combo(MENU_OPTIONS, readonly=True, default_value="Add entity", size=(23, 1), key="-MENU_COMBO-", enable_events=True)],
+        [gui.HorizontalSeparator()]
+    ]
+
     nameboxLayout = [
         [gui.Table([[]],
-            key = "-NAMEBOX-",
-            size = (40, 25), col_widths=[7, 22],
+            key = "-NAMEBOX-", 
+            size = (40, 25), col_widths=[7, 22, 5, 5],
             auto_size_columns=False,
             headings=["Initiative", "Entity Name", "Health", "DST"],
             justification="left",
@@ -63,57 +68,86 @@ def setUp():
     ]
     
     # LEFT SIDE SETUP
-    addEntityLayout = [[gui.Column([
-            [gui.Text("Enter name", background_color="firebrick4"), gui.Input(do_not_clear = False, size = (25, 1), key="-ADD_ENTITY_NAME-")],
-            [gui.Text("Enter initiative + modifier", background_color="firebrick4")],
-                [gui.Text("First box is base roll, second box is modifier.", text_color = 'grey70', background_color="firebrick4", pad=(5, 0))],
-                [gui.Text("Leave first box blank for random base roll.", text_color = 'grey70', background_color="firebrick4", pad=(5, 0))],
-                [gui.Text("Leave second box blank for +0 modifier.", text_color = 'grey70', background_color="firebrick4", pad=(5, 0))],
+    addEntityLayout = [
+            [gui.Text("Enter name"), gui.Input(do_not_clear = False, size = (25, 1), key="-ADD_ENTITY_NAME-")],
+            [gui.Text("Enter initiative + modifier")],
+                [gui.Text("First box is base roll, second box is modifier.", pad=(5, 0))],
+                [gui.Text("Leave first box blank for random base roll.", pad=(5, 0))],
+                [gui.Text("Leave second box blank for +0 modifier.", pad=(5, 0))],
                 [gui.Input(do_not_clear = False, size = (10, 1), key="-ADD_ENTITY_BASE_INIT-"), gui.Input(size = (10, 1), do_not_clear = False, key="-ADD_ENTITY_MOD_INIT-")],
-            [gui.Checkbox("Is NPC?", background_color="firebrick4", key="-NPC_CHECK-", enable_events=True)],
+            [gui.Checkbox("Is NPC?", key="-NPC_CHECK-", enable_events=True)],
             [collapsable(
                 [
                     [gui.Text("NPC Settings:")],
                     [gui.HorizontalSeparator()],
                     [gui.Text("HP"), gui.Input(do_not_clear=False, size=(10, 1), key="-NPC_HEALTH-")],
                     [gui.HorizontalSeparator()],
-                    [gui.Checkbox("Track death saving throws", key="-NPC_DEATH_SAVING_THROWS-", pad=(5, 0))],
+                    [gui.Checkbox("Track death saving throws", key="-NPC_DEATH_SAVING_THROWS-", enable_events=True, pad=(5, 0))],
                     [gui.Checkbox("Do not remove at 0HP", key="-NPC_AT_DEATH_TRACK-", pad=(5, 0))]
                 ], "-ENTITY_SETTINGS-", size=(280, 240), visible=False
             )],
-            [gui.Button("Add to initiative order", button_color="orange red", key="-ADD_TO_INIT-")]
-        ], size=(500, 800), background_color="black")
-    ]]
-    
-    leftLayout = changeLeftSide(addEntityLayout)
+            [gui.Button("Add to initiative order", key="-ADD_TO_INIT-")]
+    ]
+
+    runCombatLayout = [
+        [gui.Column([
+            [collapsable([[gui.Text("Click button to start combat:")], [gui.Button("Start Combat")]], "-START_COMBAT_LEFT-")],
+            [collapsable([[gui.Text("Must have at least 2 entities to start")]], "-START_COMBAT_ERROR-", visible=True)]
+        ], vertical_alignment="top")]
+    ]
+
+    leftLayout = topBar + [[collapsable(addEntityLayout, "--ADD_ENTITY_LAYOUT--", size=(300, 500), visible=True)]] + [[collapsable(runCombatLayout, "--RUN_COMBAT--", size=(300, 500), visible=False)]]
     
 
     layout = [
-            [gui.Column(leftLayout, background_color="firebrick4", scrollable=False, size=(300, 500), vertical_alignment="top", key="-LEFT_MENU-"),
+            [gui.Column(leftLayout, scrollable=False, size=(300, 500), vertical_alignment="top", key="-LEFT_MENU-"),
              gui.VSeparator(),
-             gui.Column(nameboxLayout, background_color="firebrick4")]
+             gui.Column(nameboxLayout)]
     ]
-    return gui.Window("DND Initiative Tracker", layout, background_color="firebrick4")
+    return gui.Window("DND Initiative Tracker", layout)
     
 
 def run(window):
     entities = []
+    currSelectedEntity = None
     flag = False
     while not flag:
         event, values = window.read()
-        print(values)
+        print("HI", event, values)
         if event == gui.WIN_CLOSED:
             terminate(window)
+
+        # ADD ENTITY TITLE:
         elif event == "-ADD_TO_INIT-" and values["-ADD_ENTITY_NAME-"] != "" and testIsIntOptional(values["-ADD_ENTITY_BASE_INIT-"]) and testIsIntOptional(values["-ADD_ENTITY_MOD_INIT-"]):
             entities = addEntity(window, entities, values["-ADD_ENTITY_NAME-"], values["-ADD_ENTITY_BASE_INIT-"], values["-ADD_ENTITY_MOD_INIT-"], None if not values["-NPC_CHECK-"] else (
                 values["-NPC_HEALTH-"], values["-NPC_DEATH_SAVING_THROWS-"]
             ))
-            print("HI")
         elif event == "-NPC_CHECK-":
             window["-ENTITY_SETTINGS-"].update(visible = values["-NPC_CHECK-"])
             window["-ADD_TO_INIT-"].update()
-        elif type(event) == tuple and event[0] == "-NAMEBOX-":
-            print("HI")
+        elif event == "-NPC_DEATH_SAVING_THROWS-":
+            window["-NPC_AT_DEATH_TRACK-"].update(text = "Do not remove on death" if values[event] else "Do not remove at 0HP")
+
+        # MANAGE COMBAT
+        elif event == "-START_COMBAT_LEFT-":
+            window["-START_COMBAT_LEFT-"].update(visible=False)
+        
+        elif event == "-MENU_COMBO-":
+            print(event)
+            if values["-MENU_COMBO-"] == MENU_OPTIONS[0]:
+                window["--ADD_ENTITY_LAYOUT--"].update(visible=True)
+                window["--RUN_COMBAT--"].update(visible=False)
+            elif values["-MENU_COMBO-"] == MENU_OPTIONS[1]:
+                window["--ADD_ENTITY_LAYOUT--"].update(visible=False)
+                window["--RUN_COMBAT--"].update(visible=True)
+
+        if len(entities) >= 2:
+            window["-START_COMBAT_ERROR-"].update(visible=False)
+            window["-START_COMBAT_LEFT-"].update(visible=True)
+        else:
+            window["-START_COMBAT_ERROR-"].update(visible=True)
+            window["-START_COMBAT_LEFT-"].update(visible=False)
+
     window.close()
 
 def addEntity(window, en, name, bI, iM, npcData):
@@ -131,13 +165,6 @@ def addEntity(window, en, name, bI, iM, npcData):
         len(getData(en)[0])
     )])
     return en
-
-def changeLeftSide(settings):
-    topBar = [
-        [gui.Text("Current Mode:", background_color="firebrick4"), gui.Combo(["Add entity", "Run Combat"], readonly=True, default_value="Add entity", size=(23, 1), background_color="indian red", text_color="black", key="-MENU_COMBO-", enable_events=True)],
-        [gui.HorizontalSeparator()]
-    ]
-    return topBar + settings
 
 def order(entities):
     i = 1
