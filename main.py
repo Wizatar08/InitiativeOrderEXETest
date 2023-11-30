8e81def2f88bd1cf8e3093ed8a5af6be61d297de
_Wizatar08 <kiyoshijim88@gmail.com>
2023-11-30 11:03:17 -0700

    Reordered the whole Add Entity layout

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import PySimpleGUI as gui
 import random
 
 MENU_OPTIONS = ["Add Entity", "Run Combat"]
+ENTITY_MENU_OPTIONS = ["Initiative and Base Stats", "Health", "Spells"]
 LEFT_SIDE_SIZE = (300, 500)
 
 class Entity:
 
-    def __init__(self, name, initiative, iModifier = 0, isNPC = False, health = None, trackDeathSavingThrows = False):
+    def __init__(self, name, initiative, iModifier = 0, isNPC = False, health = None, trackDeathSavingThrows = False, conditions=[]):
         self.name = name
         self.initiative = initiative + iModifier
         self.iModifier = iModifier
         self.isNPC = isNPC
         self.health = health
         self.trackDeathSavingThrows = trackDeathSavingThrows
+        self.conditions = conditions
+
+        self.removable = False
 
     def __str__(self) -> str:
         return f"{self.name}: {self.initiative}"
@@ -48,8 +52,8 @@ def main():
     window = setUp()
     run(window)
 
-def collapsable(layout, key, size=(None, None), visible=True, element_justification=None, background_color=None, justification=None, vertical_alignment=None, padding=None):
-    return gui.pin(gui.Column(layout, size=size, visible=visible, key=key, element_justification=element_justification, background_color=background_color, justification=justification, pad=padding))
+def collapsable(layout, key, size=(None, None), visible=True, element_justification=None, background_color=None, justification=None, vertical_alignment=None, padding=None, scrollable=None):
+    return gui.pin(gui.Column(layout, size=size, visible=visible, key=key, element_justification=element_justification, background_color=background_color, justification=justification, pad=padding, scrollable=scrollable))
 
 def setUp():
     topBar = [
@@ -70,27 +74,50 @@ def setUp():
         )]
     ]
     
+
+    
+    # -----------------------
     # LEFT SIDE SETUP
-    addEntityLayout = [
-            [gui.Text("Enter name"), gui.Input(do_not_clear = False, size = (25, 1), key="-ADD_ENTITY_NAME-")],
-            [gui.Text("Enter initiative + modifier")],
-                [gui.Text("First box is base roll, second box is modifier.", pad=(5, 0), text_color="grey80")],
-                [gui.Text("Leave first box blank for random base roll.", pad=(5, 0), text_color="grey80")],
-                [gui.Text("Leave second box blank for +0 modifier.", pad=(5, 0), text_color="grey80")],
-                [gui.Input(do_not_clear = False, size = (10, 1), key="-ADD_ENTITY_BASE_INIT-"), gui.Input(size = (10, 1), do_not_clear = False, key="-ADD_ENTITY_MOD_INIT-")],
-            [gui.Checkbox("Is NPC?", key="-NPC_CHECK-", enable_events=True)],
-            [collapsable(
-                [
-                    [gui.Text("NPC Settings:")],
-                    [gui.HorizontalSeparator()],
-                    [gui.Text("HP"), gui.Input(do_not_clear=False, size=(10, 1), key="-NPC_HEALTH-")],
-                    [gui.HorizontalSeparator()],
-                    [gui.Checkbox("Track death saving throws", key="-NPC_DEATH_SAVING_THROWS-", enable_events=True, pad=(5, 0))],
-                    [gui.Checkbox("Do not remove at 0HP", key="-NPC_AT_DEATH_TRACK-", pad=(5, 0))]
-                ], "-ENTITY_SETTINGS-", size=(280, 240), visible=False, padding=(0,0)
-            )],
-            [gui.Button("Add to initiative order", key="-ADD_TO_INIT-")]
+    # -----------------------
+
+    initiativeMenu = [
+        [gui.Text("Enter name"), gui.Input(size = (25, 1), key="-ADD_ENTITY_NAME-")],
+        [gui.Text("Enter initiative + modifier")],
+        [gui.Text("First box is base roll, second box is modifier.", pad=(5, 0), text_color="grey80")],
+        [gui.Text("Leave first box blank for random base roll.", pad=(5, 0), text_color="grey80")],
+        [gui.Text("Leave second box blank for +0 modifier.", pad=(5, 0), text_color="grey80")],
+        [gui.Input(size = (10, 1), key="-ADD_ENTITY_BASE_INIT-"), gui.Input(size = (10, 1), key="-ADD_ENTITY_MOD_INIT-")],
+        [gui.Checkbox("Is NPC?", key="-NPC_CHECK-")],
     ]
+
+    healthMenu = [
+        [gui.Text("HP"), gui.Input(size=(10, 1), key="-NPC_HEALTH-")],
+        [gui.HorizontalSeparator()],
+        [gui.Checkbox("Track death saving throws", key="-NPC_DEATH_SAVING_THROWS-", enable_events=True, pad=(5, 0))],
+        [gui.Checkbox("Do not remove at 0HP", key="-NPC_AT_DEATH_TRACK-", pad=(5, 0))],
+        [gui.Text("Conditions: "), gui.Input(size = (18, 1), key="-CONDITIONS_TEXT-"), gui.Button("+", size=(2,1), key="-CONDITIONS_ADD-"), gui.Button("-", size=(2,1), key="-CONDITIONS_REMOVE-")],
+            [gui.Listbox([], size=(38, 5), key="-CONDITIONS_LISTBOX-")]
+    ]
+
+    spellsMenu = [
+        [gui.Text("0th: "), gui.Input(size=[2, 1], key="-ADD_SPELLS_0-"), gui.Text("1st: "), gui.Input(size=[2, 1], key="-ADD_SPELLS_1-"), gui.Text("2nd: "), gui.Input(size=[2, 1], key="-ADD_SPELLS_2-"), gui.Text("3rd: "), gui.Input(size=[2, 1], key="-ADD_SPELLS_3-")],
+        [gui.Text("4th: "), gui.Input(size=[2, 1], key="-ADD_SPELLS_4-"), gui.Text("5th: "), gui.Input(size=[2, 1], key="-ADD_SPELLS_5-"), gui.Text("6th: "), gui.Input(size=[2, 1], key="-ADD_SPELLS_6-"), gui.Text("7th: "), gui.Input(size=[2, 1], key="-ADD_SPELLS_7-")],
+        [gui.Text("8th: "), gui.Input(size=[2, 1], key="-ADD_SPELLS_8-"), gui.Text("9th: "), gui.Input(size=[2, 1], key="-ADD_SPELLS_9-")]
+    ]
+
+
+    addEntityLayout = [
+            [gui.Combo(ENTITY_MENU_OPTIONS, readonly=True, default_value=ENTITY_MENU_OPTIONS[0], size=(23, 1), key="-ADD_ENTITY_MENU_COMBO-", enable_events=True)],
+            [gui.Button("Add to initiative order", key="-ADD_TO_INIT-")],
+            [gui.HorizontalSeparator()],
+            [collapsable(initiativeMenu, "-ENTITY_INITIATIVE_MENU-")],
+            [collapsable(healthMenu, "-ENTITY_HEALTH_MENU-", visible=False)],
+            [collapsable(spellsMenu, "-ENTITY_SPELLS_MENU-", visible=False)]
+    ]
+
+    # ---------------------------
+    # COMBAT LAYOUT
+    # --------------------------
 
     runCombatLayout = [
         [gui.Column([
@@ -106,7 +133,7 @@ def setUp():
     
 
     layout = [
-            [gui.Column(leftLayout, scrollable=False, size=LEFT_SIDE_SIZE, vertical_alignment="top", key="-LEFT_MENU-", pad=(0,0)),
+            [gui.Column(leftLayout, size=(350, 500), vertical_alignment="top", key="-LEFT_MENU-", pad=(0,0)),
              gui.VSeparator(),
              gui.Column(nameboxLayout)]
     ]
@@ -118,22 +145,42 @@ def run(window):
     currEntityIndex = None
     currSelectedEntityIndex = None
     flag = False
+
+    # SET UP VARIABLES
+    conditions = []
+
     while not flag:
         event, values = window.read()
         
         if event == gui.WIN_CLOSED:
             terminate(window)
 
-        # ADD ENTITY TITLE:
-        elif event == "-ADD_TO_INIT-" and values["-ADD_ENTITY_NAME-"] != "" and testIsIntOptional(values["-ADD_ENTITY_BASE_INIT-"]) and testIsIntOptional(values["-ADD_ENTITY_MOD_INIT-"]) and (not values["-NPC_CHECK-"] or isInt(values["-NPC_HEALTH-"])):
-            entities = addEntity(window, entities, values["-ADD_ENTITY_NAME-"], values["-ADD_ENTITY_BASE_INIT-"], values["-ADD_ENTITY_MOD_INIT-"], None if not values["-NPC_CHECK-"] else (
-                values["-NPC_HEALTH-"], values["-NPC_DEATH_SAVING_THROWS-"] 
-            ))
-        elif event == "-NPC_CHECK-":
-            window["-ENTITY_SETTINGS-"].update(visible = values["-NPC_CHECK-"])
-            window["-ADD_TO_INIT-"].update()
+        elif event == "-ADD_ENTITY_MENU_COMBO-":
+            if values["-ADD_ENTITY_MENU_COMBO-"] == ENTITY_MENU_OPTIONS[0]:
+                window["-ENTITY_INITIATIVE_MENU-"].update(visible=True)
+                window["-ENTITY_HEALTH_MENU-"].update(visible=False)
+                window["-ENTITY_SPELLS_MENU-"].update(visible=False)
+            elif values["-ADD_ENTITY_MENU_COMBO-"] == ENTITY_MENU_OPTIONS[1]:
+                window["-ENTITY_INITIATIVE_MENU-"].update(visible=False)
+                window["-ENTITY_HEALTH_MENU-"].update(visible=True)
+                window["-ENTITY_SPELLS_MENU-"].update(visible=False)
+            elif values["-ADD_ENTITY_MENU_COMBO-"] == ENTITY_MENU_OPTIONS[2]:
+                window["-ENTITY_INITIATIVE_MENU-"].update(visible=False)
+                window["-ENTITY_HEALTH_MENU-"].update(visible=False)
+                window["-ENTITY_SPELLS_MENU-"].update(visible=True)
+
+
+
+        # INITIAITIVE:
+        elif event == "-ADD_TO_INIT-" and values["-ADD_ENTITY_NAME-"] != "" and testIsIntOptional(values["-ADD_ENTITY_BASE_INIT-"]) and testIsIntOptional(values["-ADD_ENTITY_MOD_INIT-"]):
+            entities = addEntity(window, entities, name=values["-ADD_ENTITY_NAME-"], bI=values["-ADD_ENTITY_BASE_INIT-"], iM=values["-ADD_ENTITY_MOD_INIT-"], conditions=conditions, isNPC= values["-NPC_CHECK-"], health=values["-NPC_HEALTH-"], dst=values["-NPC_DEATH_SAVING_THROWS-"])
         elif event == "-NPC_DEATH_SAVING_THROWS-":
             window["-NPC_AT_DEATH_TRACK-"].update(text = "Do not remove on death" if values[event] else "Do not remove at 0HP")
+
+        # SPELLS
+        elif event == "-CONDITIONS_ADD-":
+            conditions += [values["-CONDITIONS_TEXT-"]]
+            window["-CONDITIONS_LISTBOX-"].update(conditions)
 
         # MANAGE COMBAT
         elif event == "-START_COMBAT_BUTTON-":
@@ -159,19 +206,17 @@ def run(window):
             else:
                 window["-START_COMBAT_ERROR-"].update(visible=True)
                 window["-START_COMBAT_CONFIRM-"].update(visible=False)
-        print(event, values)
+            
+        #print(event, values)
 
     window.close()
 
-def addEntity(window, en, name, bI, iM, npcData):
-    if npcData == None:
-        e = Entity(name, random.randint(1, 20) if bI == "" else int(bI), iModifier = setOptionalIntParam(iM))
-    else:
-        e = Entity(name, random.randint(1, 20) if bI == "" else int(bI), iModifier = setOptionalIntParam(iM),
-            isNPC = True,
-            health = int(npcData[0]) if isPositiveInt(npcData[0]) else None,
-            trackDeathSavingThrows = npcData[1]
-        )
+def addEntity(window, en, name, bI, iM, conditions, isNPC, health, dst):
+    e = Entity(name, random.randint(1, 20) if bI == "" else int(bI), iModifier = setOptionalIntParam(iM), conditions=conditions,
+        isNPC = isNPC,
+        health = int(health) if isPositiveInt(health) else None,
+        trackDeathSavingThrows = dst,
+    )
     en.append(e)
     en = order(en)
     window['-NAMEBOX-'].update([en[i].forListboxString() for i in range(
